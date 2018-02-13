abf45997da24c964a265885faeecad869a58e8cb
Dmitry <akhremdzmitry@gmail.com>
2018-02-13 22:00:04 +0300

    Added simple method to App and test AppTest

--- src/main/java/com/epam/brest/cource/App.java
+++ src/main/java/com/epam/brest/cource/App.java
@@ -2,12 +2,14 @@ package com.epam.brest.cource;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+
+    public int sum(int a, int b){
+        return a+b;
     }
 }
--- src/test/java/com/epam/brest/cource/AppTest.java
+++ src/test/java/com/epam/brest/cource/AppTest.java
@@ -1,38 +1,39 @@
 package com.epam.brest.cource;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class AppTest {
+
+    @Test
+    public void sum() {
+        int expected = 10;
+        int actual = new App().sum(5, 5);
+        Assert.assertEquals(expected, actual);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void main(){
+        String expected = "Hello World!\n";
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        //save the real System.out
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        //set fake System.out
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        //run App
+        App.main(null);
+
+        //restore System.out
+        System.setOut(defaultPrintStream);
+
+        //testing
+        Assert.assertEquals(expected,byteArrayOutputStream.toString());
+
     }
 }
