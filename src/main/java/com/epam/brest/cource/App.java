78162579d9ca2682abaadac3c823dd2b9b695c43
Dmitry <akhremdzmitry@gmail.com>
2018-02-15 00:08:22 +0300

    Connect to DB

--- src/main/java/com/epam/brest/cource/App.java
+++ src/main/java/com/epam/brest/cource/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        DBUtils dbUtils = new DBUtils();
     }
 
     public int sum(int a, int b){
