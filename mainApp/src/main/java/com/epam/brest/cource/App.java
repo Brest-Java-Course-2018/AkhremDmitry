e1625bc53cb5b86d7156ce93dc15279921963c62
Dmitry <akhremdzmitry@gmail.com>
2018-02-19 20:57:07 +0300

    added maven plugins

--- mainApp/src/main/java/com/epam/brest/cource/App.java
+++ mainApp/src/main/java/com/epam/brest/cource/App.java
@@ -7,16 +7,17 @@ import java.sql.SQLException;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(final String[] args)
+            throws SQLException, ClassNotFoundException {
 
         System.out.println("Hello World!");
         DBUtils dbUtils = new DBUtils();
 
         Connection connection = dbUtils.getConnection();
         dbUtils.createUserTable(connection);
-        dbUtils.addUser(connection,"admin", "pass", "user_admin");
-        dbUtils.addUser(connection,"admin2", "pass2", "user_admin2");
-        dbUtils.addUser(connection,"admin3", "pass3", "user_admin3");
+        dbUtils.addUser(connection, "admin", "pass", "user_admin");
+        dbUtils.addUser(connection, "admin2", "pass2", "user_admin2");
+        dbUtils.addUser(connection, "admin3", "pass3", "user_admin3");
 
         dbUtils.getUsers(connection);
 
@@ -24,13 +25,14 @@ public class App {
 
         dbUtils.getUsers(connection);
 
-        dbUtils.udateUserById(connection,1,"felix","cat","superCat");
+        dbUtils.udateUserById(connection, 1, "felix", "cat", "superCat");
 
         dbUtils.getUsers(connection);
 
     }
 
-    public int sum(int a, int b){
-        return a+b;
+
+    public final int sum(final int a, final int b) {
+        return a + b;
     }
 }
