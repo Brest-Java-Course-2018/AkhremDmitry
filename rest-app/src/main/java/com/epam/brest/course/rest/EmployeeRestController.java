54887fc22fa76329a0e498b5a589dc6f3bf1c1f7
Dmitry <akhremdzmitry@gmail.com>
2018-03-22 23:51:06 +0300

    Added some methods in EmployeeRestController

--- rest-app/src/main/java/com/epam/brest/course/rest/EmployeeRestController.java
+++ rest-app/src/main/java/com/epam/brest/course/rest/EmployeeRestController.java
@@ -5,10 +5,7 @@ import com.epam.brest.course.service.EmployeeService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -34,5 +31,26 @@ public class EmployeeRestController {
     Employee getEmployeeById(@PathVariable(value = "id") Integer id){
         LOGGER.debug("getEmployeeById({})", id);
         return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping(value = "/employees")
+    @ResponseStatus(HttpStatus.CREATED)
+    Employee addEmployee(@RequestBody Employee employee){
+        LOGGER.debug("addEmployee({})", employee);
+        return employeeService.addEmployee(employee);
+    }
+
+    @DeleteMapping(value = "/employees/{id}")
+    @ResponseStatus(HttpStatus.FOUND)
+    void deleteEmployeeById(@PathVariable(value = "id") Integer employeeId){
+        LOGGER.debug("deleteEmployeeById({})", employeeId);
+        employeeService.deleteEmployeeById(employeeId);
+    }
+
+    @PutMapping(value = "/employees")
+    @ResponseStatus(HttpStatus.FOUND)
+    void updateEmployee(@RequestBody Employee employee){
+        LOGGER.debug("updateEmployee({})", employee);
+        employeeService.updateEmployee(employee);
     }
 }
