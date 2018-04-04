4f809a2cd4937823695d81c8ddc7d9e9a7492091
Dmitry <akhremdzmitry@gmail.com>
2018-04-04 17:55:52 +0300

    Added ErrorHandler and an example curl request

--- rest-server/src/main/java/com/epam/brest/course/rest/CarRestController.java
+++ rest-server/src/main/java/com/epam/brest/course/rest/CarRestController.java
@@ -24,12 +24,14 @@ public class CarRestController {
         this.carService = carService;
     }
 
+    //curl -v localhost:8090/carsDto
     @GetMapping(value = "/carsDto")
     public final Collection<CarDto> getAllCarsDto() {
         LOGGER.debug("getAllCarsDto()");
         return carService.getAllCarsDto();
     }
 
+    //curl -v localhost:8090/cars/1
     @GetMapping(value = "/cars/{id}")
     @ResponseStatus(HttpStatus.FOUND)
     public final Car getCarById(@PathVariable final int id) {
@@ -37,6 +39,7 @@ public class CarRestController {
         return carService.getCarById(id);
     }
 
+    //curl -H "Content-Type: application/json" -X POST -d '{"registrationPlate":"3333 AA-2","description":"Ambulance"}' -v localhost:8090/cars
     @PostMapping(value = "/cars")
     @ResponseStatus(HttpStatus.CREATED)
     public final Car addCar(@RequestBody final Car car) {
@@ -44,18 +47,21 @@ public class CarRestController {
         return carService.addCar(car);
     }
 
+    //curl -H "Content-Type: application/json" -X PUT -d '{"carId":1,"registrationPlate":"3333 AA-2","description":"Ambulance"}' -v localhost:8090/cars
     @PutMapping(value = "/cars")
     public final void updateCar(@RequestBody final Car car) {
         LOGGER.debug("updateCar({})", car);
         carService.updateCar(car);
     }
 
+    //curl -X "DELETE" localhost:8090/cars/1
     @DeleteMapping(value = "/cars/{id}")
     public final void deleteCarById(@PathVariable final int id) {
         LOGGER.debug("deleteCarById({})", id);
         carService.deleteCarById(id);
     }
 
+    //curl -v localhost:8090/cars
     @GetMapping(value = "/cars")
     public final Collection<CarDtoWithCrew> getAllCarsDtoWithCrew() {
         LOGGER.debug("getAllCarsDtoWithCrew()");
